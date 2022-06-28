544bbd8af4cf3abc78aaff4de96f53b777418864
Jeevanjot Singh Vital <60998339+devindirt@users.noreply.github.com>
2022-06-28 17:58:09 +0530

    Update sha256.js

--- src/sha256.js
+++ src/sha256.js
@@ -1,15 +1,12 @@
 const crypto = require('crypto')
 
-function toBuffer256(number) {
-  return Buffer.from(number.toString(16).padStart(512, '0'), 'hex')
-}
 
 function sha256(left, right) {
   return crypto
     .createHash('sha256')
-    .update(toBuffer256(left))
-    .update(toBuffer256(right))
+    .update(left.substring(0, 64).concat(right.substring(0, 64)))
     .digest('hex')
 }
+
 
 module.exports = sha256
